fb936e434223063d2a9a9a87e0e8274902e80b77
Artem Apostatov <alexeevdev@yahoo.com>
2022-08-27 23:48:01 +0300

    feat: add config to repo

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://blog.twitter.com/engineering/en_us/blog.rss" "https://overreacted.io/rss.xml" "https://www.nngroup.com/feed/rss/" "https://hackernoon.com/feed" "https://hnrss.org/frontpage" "http://hnbest.heroku.com/rss" "https://world.hey.com/jason/feed.atom" "https://martinfowler.com/feed.atom")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
