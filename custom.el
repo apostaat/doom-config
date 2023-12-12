a1c920a25c74aad0f121889213d1ff716128bfe1
Artem Apostatov <alexeevdev@yahoo.com>
2023-12-12 18:08:35 +0400

    add completion engine that works smoothly and lsp works normally

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" default))
+ '(elfeed-feeds
+   '("https://blog.twitter.com/engineering/en_us/blog.rss" "https://overreacted.io/rss.xml" "https://www.nngroup.com/feed/rss/" "https://hackernoon.com/feed" "https://hnrss.org/frontpage" "http://hnbest.heroku.com/rss" "https://world.hey.com/jason/feed.atom" "https://martinfowler.com/feed.atom")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
